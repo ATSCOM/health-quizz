1dab250acbba323e91d71f82b615605a240919a6
Jorgetecno3898@hotmail.com <Jorgetecno3898@hotmail.com>
2021-11-16 11:26:01 -0500

    fix size cards

--- resources/views/layouts/suggestion.blade.php
+++ resources/views/layouts/suggestion.blade.php
@@ -5,10 +5,12 @@
 
     @foreach ($quizzes as $quiz)
 
-        <a role="button" class="btn btn-lg card" href="{{ "quizz/".$quiz->id }}" style="width: 10rem;">
-            <img src="{{ asset('img/bandera-uceva.png') }}" class="card-img-top img-thumbnail" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">{{ $quiz->description }}</h5>
+        <a role="button" class="btn btn-lg" href="{{ "quizz/".$quiz->id }}">
+            <div class="card mb-3" style="width: 10rem;">
+                <img src="{{ asset('img/bandera-uceva.png') }}" class="card-img-top img-thumbnail" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $quiz->description }}</h5>
+                </div>
             </div>
         </a>
 
