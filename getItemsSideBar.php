0d7f43b03d7127ec52c0bd54e5e68de9d13b3b3e
Jorgetecno3898@hotmail.com <Jorgetecno3898@hotmail.com>
2021-11-18 00:32:43 -0500

    Add vendor to view

--- getItemsSideBar.php
+++ getItemsSideBar.php
@@ -2,8 +2,8 @@
 $request = $_REQUEST['items'];
 if(isset($request)){
   $host = 'localhost';
-  $user = 'yxd';
-  $password = 'AmHt2018Ab.';
+  $user = 'jl';
+  $password = '911';
   $dbName = 'health';
   $table = 'categories';
   $pdo = new PDO("mysql:host=$host;dbname=$dbName", $user, $password);
