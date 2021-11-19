e548c9a5fc1705ad79629be6ee57efcdce303e0b
Jorgetecno3898@hotmail.com <Jorgetecno3898@hotmail.com>
2021-11-19 04:04:45 -0500

    fix url
    fix a problem with url of cards

--- resources/views/layouts/suggestion.blade.php
+++ resources/views/layouts/suggestion.blade.php
@@ -5,7 +5,7 @@
 
     @foreach ($quizzes as $quiz)
 
-        <a role="button" class="btn btn-lg" href="{{ "quizz/".$quiz->id }}">
+        <a role="button" class="btn btn-lg" href="{{ URL::asset('quizz/'.$quiz->id) }}">
             <div class="card mb-3" style="width: 10rem;">
                 <img src="{{ asset('img/bandera-uceva.png') }}" class="card-img-top img-thumbnail" alt="...">
                 <div class="card-body">
