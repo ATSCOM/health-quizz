07e7a042658f4c76eb0df7a87bdcab9180e108cf
Jorgetecno3898@hotmail.com <Jorgetecno3898@hotmail.com>
2021-11-20 18:33:36 -0500

    fix routes login, register, start and suggestion

--- resources/views/layouts/suggestion.blade.php
+++ resources/views/layouts/suggestion.blade.php
@@ -7,7 +7,7 @@
 
         <a role="button" class="btn btn-lg" href="{{ URL::asset('quizz/'.$quiz->id) }}">
             <div class="card mb-3" style="width: 10rem;">
-                <img src="{{ asset('img/bandera-uceva.png') }}" class="card-img-top img-thumbnail" alt="...">
+                <img src="{{ asset('images/pages/bandera-uceva.png') }}" class="card-img-top img-thumbnail" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">{{ $quiz->description }}</h5>
                 </div>
