a3b5003f4247592bb56107e72b8a8f008f0f0e7b
Jorgetecno3898@hotmail.com <Jorgetecno3898@hotmail.com>
2021-12-06 18:51:03 -0500

    Fix view template quizz with image

--- resources/views/quiz/template/quizz.blade.php
+++ resources/views/quiz/template/quizz.blade.php
@@ -1,16 +1,37 @@
 <div class="row">
-    <div class="col-12 col-md-6">
-        <p class="font-weight-bold">{{ $question }}</p>
-        <img src="{{ asset('/images/quizzes/default.png') }}" class="img-fluid card-img-top" alt="" width="50%">
-    </div>
-    <div class="col-12 col-md-6">
-        <p class="font-weight-bold">Seleccione una opción:</p>
-        @foreach($options as $option)
-        <button type="button" onclick="document.getElementById('responseQuiz').value = {{$option['id']}};" class="btn btn-primary btn-lg btn-block">{{$option['description']}}</button>
-        @endforeach
 
-        <div class="form-floating mt-3">
-            <textarea class="form-control" placeholder="Justifique su respuesta" id="floatingTextarea" readonly>{{ is_null($response) ? '' : $response }}</textarea>
+    @if ($image != 'NULL' && $image != '')
+
+        <div class="col-12 col-md-6">
+            <p class="font-weight-bold">{{ $question }}</p>
+            <img src="{{ asset($image) }}" class="img-fluid card-img-top" alt="" width="50%">
         </div>
-    </div>
+        <div class="col-12 col-md-6">
+            <p class="font-weight-bold">Seleccione una opción:</p>
+            @foreach($options as $option)
+            <button type="button" onclick="document.getElementById('responseQuiz').value = {{$option['id']}};" class="btn btn-primary btn-lg btn-block">{{$option['description']}}</button>
+            @endforeach
+
+            <div class="form-floating mt-3">
+                <textarea class="form-control" placeholder="Justifique su respuesta" id="floatingTextarea" readonly>{{ is_null($response) ? '' : $response }}</textarea>
+            </div>
+        </div>
+
+    @else
+
+        <div class="col-12 col-md-12">
+            <p class="font-weight-bold">{{ $question }}</p>
+            <p class="font-weight-bold">Seleccione una opción:</p>
+            @foreach($options as $option)
+            <button type="button" onclick="document.getElementById('responseQuiz').value = {{$option['id']}};" class="btn btn-primary btn-lg btn-block">{{$option['description']}}</button>
+            @endforeach
+
+            <div class="form-floating mt-3">
+                <textarea class="form-control" placeholder="Justifique su respuesta" id="floatingTextarea" readonly>{{ is_null($response) ? '' : $response }}</textarea>
+            </div>
+        </div>
+
+    @endif
+
+
 </div>
