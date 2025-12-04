484d51ed1459023646feb6bf7991e41d8b168ce9
Yesid Hernandez <yesid.hernandez@alegra.com>
2025-12-04 14:10:11 -0500

    feat: Update database password in personal.php to use a default value for better security

--- config/personal.php
+++ config/personal.php
@@ -1,7 +1,7 @@
 <?php
 ## INFO DB - Ahora usando variables de entorno de Laravel
 $DB_USER = env('DB_USERNAME', 'health_user');
-$DB_PASSWORD = env('DB_PASSWORD', '');
+$DB_PASSWORD = env('DB_PASSWORD', 'secret');
 $DB_NAME = env('DB_DATABASE', 'health_quizz');
 
 ## INFO URI - Para Docker
